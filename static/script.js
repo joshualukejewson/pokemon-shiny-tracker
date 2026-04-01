7f51c9a64febb47f7cfab6523735fcc789d55476
joshualukejewson <joshuajewson@gmail.com>
2026-04-01 16:29:52 +1000

    added reset button and added extra normal version of the sprite next to the shiny for comparisons.\

--- static/script.js
+++ static/script.js
@@ -1,7 +1,13 @@
-const encounterButton = document.querySelector("[name='encounter-button']");
+const encounterButton = document.getElementById("encounter-button");
 const encounterCount = document.getElementById("encounters-count");
 let count = 0;
 
 encounterButton.addEventListener("click", function() {
     encounterCount.innerHTML = ++count;
 });
+
+const resetButton = document.getElementById("reset-button");
+
+resetButton.addEventListener("click", function () {
+    encounterCount.innerHTML = count = 0;
+})
