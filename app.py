533e0d9939cdf4eecc79c4737c7d28575f718f68
joshualukejewson <joshuajewson@gmail.com>
2026-04-02 17:41:31 +1000

    undoing recent database changes as procedure was outdated. following updated protocol in future.

--- app.py
+++ app.py
@@ -15,19 +15,12 @@ development server when run directly.
 from flask import Flask, render_template, request, url_for
 from services.pokeapi import get_individual_pokemon_data
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import UserMixin
 
 # Create the Flask application instance
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
 app.config["SECRET_KEY"] = "bulbasauristhebestgen1starter"
 db = SQLAlchemy(app)
-
-
-class User(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), nullable=False)
-    password = db.Column(db.String(100), nullable=False)
 
 
 @app.route("/", methods=["GET", "POST"])
@@ -66,6 +59,4 @@ def hunt():
 
 
 if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
     app.run(debug=True)
