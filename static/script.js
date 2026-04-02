477ae482183931bd39e4e5ea441a4df3ff0938a3
joshualukejewson <joshuajewson@gmail.com>
2026-04-02 16:43:18 +1000

    deleted base template, added decrement button and styled page.

--- static/script.js
+++ static/script.js
@@ -1,10 +1,16 @@
 const encounterButton = document.getElementById("encounter-button");
+const decrementButton = document.getElementById("decrement-button");
 const encounterCount = document.getElementById("encounters-count");
 const resetButton = document.getElementById("reset-button");
 let count = 0;
 
 encounterButton.addEventListener("click", function() {
     encounterCount.innerHTML = ++count;
+});
+decrementButton.addEventListener("click", function() {
+    if (count > 0) {
+    encounterCount.innerHTML = --count;
+    }
 });
 
 resetButton.addEventListener("click", function () {
