efc094d5f13f8364de46bb4053cdd6f502824bce
joshualukejewson <joshuajewson@gmail.com>
2026-04-02 16:17:01 +1000

    deleted template login page and minor ui tweaks.

--- static/script.js
+++ static/script.js
@@ -1,12 +1,11 @@
 const encounterButton = document.getElementById("encounter-button");
 const encounterCount = document.getElementById("encounters-count");
+const resetButton = document.getElementById("reset-button");
 let count = 0;
 
 encounterButton.addEventListener("click", function() {
     encounterCount.innerHTML = ++count;
 });
-
-const resetButton = document.getElementById("reset-button");
 
 resetButton.addEventListener("click", function () {
     encounterCount.innerHTML = count = 0;
